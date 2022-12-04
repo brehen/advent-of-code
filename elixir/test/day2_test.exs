1a21280f4b1c4b8169af8f091cbaa381d376ed5b
brehen <mkluften93@gmail.com>
2022-12-04 12:34:02 +0100

    🎨 Format

--- elixir/test/day2_test.exs
+++ elixir/test/day2_test.exs
@@ -9,62 +9,81 @@ defmodule AOC.Day2Test do
 
   test "interprets simple input correctly" do
     Day2.get_rounds(@simple)
-    |> Day2.get_mapped_rounds_1
+    |> Day2.get_mapped_rounds_1()
     |> assert_lists_equal([["rock", "paper"], ["paper", "rock"], ["scissors", "scissors"]])
   end
 
   # score == outcome + chosen move
   test "finds opposing moves correctly" do
-    score1 = ["A", "Y"]
-    |> Day2.get_mapped_moves()
-    |> Day2.get_score_from_round()
+    score1 =
+      ["A", "Y"]
+      |> Day2.get_mapped_moves()
+      |> Day2.get_score_from_round()
+
     assert score1 == 6 + 2
-    score2 = ["B", "X"]
-    |> Day2.get_mapped_moves()
-    |> Day2.get_score_from_round()
+
+    score2 =
+      ["B", "X"]
+      |> Day2.get_mapped_moves()
+      |> Day2.get_score_from_round()
+
     assert score2 == 0 + 1
-    score3 = ["C", "Z"]
-    |> Day2.get_mapped_moves()
-    |> Day2.get_score_from_round()
+
+    score3 =
+      ["C", "Z"]
+      |> Day2.get_mapped_moves()
+      |> Day2.get_score_from_round()
+
     assert score3 == 3 + 3
-    score4 = ["B", "Z"]
-    |> Day2.get_mapped_moves()
-    |> Day2.get_score_from_round()
+
+    score4 =
+      ["B", "Z"]
+      |> Day2.get_mapped_moves()
+      |> Day2.get_score_from_round()
+
     assert score4 == 6 + 3
-    score5 = ["A", "X"]
-    |> Day2.get_mapped_moves()
-    |> Day2.get_score_from_round()
+
+    score5 =
+      ["A", "X"]
+      |> Day2.get_mapped_moves()
+      |> Day2.get_score_from_round()
+
     assert score5 == 3 + 1
-    score6 = ["B", "Y"]
-    |> Day2.get_mapped_moves()
-    |> Day2.get_score_from_round()
+
+    score6 =
+      ["B", "Y"]
+      |> Day2.get_mapped_moves()
+      |> Day2.get_score_from_round()
+
     assert score6 == 3 + 2
   end
 
-
-
   test "Outcome of round gives correct score" do
-    rounds = Day2.get_rounds(@simple)
-    |> Day2.get_mapped_rounds_1
-    |> Enum.map(&Day2.get_score_from_round/1)
-    |> Enum.sum
+    rounds =
+      Day2.get_rounds(@simple)
+      |> Day2.get_mapped_rounds_1()
+      |> Enum.map(&Day2.get_score_from_round/1)
+      |> Enum.sum()
 
-      assert rounds == 15
+    assert rounds == 15
   end
 
   test "returns correct for larger set as well" do
-    rounds = Day2.get_rounds(@complex)
-    |> Day2.get_mapped_rounds_1
-    |> Enum.map(&Day2.get_score_from_round/1)
-    |> Enum.sum
+    rounds =
+      Day2.get_rounds(@complex)
+      |> Day2.get_mapped_rounds_1()
+      |> Enum.map(&Day2.get_score_from_round/1)
+      |> Enum.sum()
+
     assert rounds == 13268
   end
 
   test "but there was a twist" do
-    rounds = Day2.get_rounds(@complex)
-    |> Day2.get_mapped_rounds_2
-    |> Enum.map(&Day2.get_score_from_round/1)
-    |> Enum.sum
+    rounds =
+      Day2.get_rounds(@complex)
+      |> Day2.get_mapped_rounds_2()
+      |> Enum.map(&Day2.get_score_from_round/1)
+      |> Enum.sum()
 
     assert rounds == 15508
   end
--- elixir/test/day3_test.exs
+++ elixir/test/day3_test.exs
@@ -20,49 +20,54 @@ defmodule AOC.Day3Test do
   end
 
   test "finds common item type" do
-      @simple
-      |> Day3.get_compartments()
-      |> Day3.get_common_item_type()
-      |> assert_lists_equal(["p", "L", "P", "v", "t", "s"])
+    @simple
+    |> Day3.get_compartments()
+    |> Day3.get_common_item_type()
+    |> assert_lists_equal(["p", "L", "P", "v", "t", "s"])
   end
 
   test "priority values mapped correctly" do
-   assert 157 == ["p", "L", "P", "v", "t", "s"]
-    |> Enum.map(&Day3.get_priority_value/1)
-    |> Enum.sum
+    assert 157 ==
+             ["p", "L", "P", "v", "t", "s"]
+             |> Enum.map(&Day3.get_priority_value/1)
+             |> Enum.sum()
   end
 
   test "finds sum from simple input" do
-    assert 157 == @simple
-    |> Day3.get_compartments
-    |> Day3.get_common_item_type
-    |> Enum.map(&Day3.get_priority_value/1)
-    |> Enum.sum
+    assert 157 ==
+             @simple
+             |> Day3.get_compartments()
+             |> Day3.get_common_item_type()
+             |> Enum.map(&Day3.get_priority_value/1)
+             |> Enum.sum()
   end
 
   test "find sum from complex input" do
-    assert 7848 == @complex
-    |> Day3.get_compartments
-    |> Day3.get_common_item_type
-    |> Enum.map(&Day3.get_priority_value/1)
-    |> Enum.sum
+    assert 7848 ==
+             @complex
+             |> Day3.get_compartments()
+             |> Day3.get_common_item_type()
+             |> Enum.map(&Day3.get_priority_value/1)
+             |> Enum.sum()
   end
 
   # Part two
 
   test "determine common items and their points for each group" do
-    assert 70 == @simple
-    |> Day3.get_rucksack_groups
-    |> Day3.get_common_item_type_across_group
-    |> Enum.map(&Day3.get_priority_value/1)
-    |> Enum.sum
+    assert 70 ==
+             @simple
+             |> Day3.get_rucksack_groups()
+             |> Day3.get_common_item_type_across_group()
+             |> Enum.map(&Day3.get_priority_value/1)
+             |> Enum.sum()
   end
 
   test "determine common items and their points for each group in complex" do
-    assert 2616 == @complex
-    |> Day3.get_rucksack_groups
-    |> Day3.get_common_item_type_across_group
-    |> Enum.map(&Day3.get_priority_value/1)
-    |> Enum.sum
+    assert 2616 ==
+             @complex
+             |> Day3.get_rucksack_groups()
+             |> Day3.get_common_item_type_across_group()
+             |> Enum.map(&Day3.get_priority_value/1)
+             |> Enum.sum()
   end
 end
