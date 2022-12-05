a4db7d144bad828d70c0303fd9b650ff14fe452e
brehen <mkluften93@gmail.com>
2022-12-05 18:18:56 +0100

    ✨ Implement part 1 of day 5

--- elixir/lib/day5.ex
+++ elixir/lib/day5.ex
@@ -1,23 +1,71 @@
 defmodule AOC.Day5 do
   import AOC.Utils, only: [read_input: 1]
 
-  def get_gnomes(file_path) do
+  def get_input_parts(file_path) do
     read_input(file_path)
     |> String.split(~r/\n\n/, trim: true)
-    |> Enum.map(&String.split(&1, ~r/\n/, trim: true))
-    |> Enum.map(fn gnome -> Enum.map(gnome, &String.to_integer/1) end)
-    |> Enum.map(&Enum.sum/1)
   end
 
-  def get_highest(result) do
-    result |> Enum.max()
+  def parse_crane(crane) do
+    crane
+    |> String.split(~r/\n/, trim: true)
+    |> Enum.map(fn line ->
+      line =
+        line
+        |> String.split("", trim: true)
+
+      Enum.drop_every(["" | line], 4)
+      |> Enum.chunk_every(3)
+      |> Enum.map(fn [_, two, _] -> two end)
+    end)
+    |> List.zip()
+    |> Enum.map(&Tuple.to_list/1)
+    |> Enum.map(&Enum.filter(&1, fn c -> c != " " end))
+    |> Enum.map(&Enum.drop(&1, -1))
   end
 
-  def get_three_highest_sum(gnomes) do
-    gnomes
-    |> Enum.sort()
-    |> Enum.reverse()
-    |> Enum.slice(0..2)
-    |> Enum.sum()
+  def parse_movements(moves) do
+    moves
+    |> String.split(~r/\n/, trim: true)
+    |> Enum.map(fn line ->
+      line
+      |> String.split(~r/\s/, trim: true)
+      |> Enum.drop_every(2)
+      |> Enum.map(&String.to_integer/1)
+    end)
+  end
+
+  def parse_input_parts([crane, moves]),
+    do: {
+      parse_crane(crane),
+      parse_movements(moves)
+    }
+
+  def perform_move({crane, [0, _from, _to]}) do
+    crane
+  end
+
+  def perform_move({crane, [move, from, to]}) do
+    [from_at | new_from] = Enum.at(crane, from - 1)
+    to_at = Enum.at(crane, to - 1)
+    new_to = [from_at | to_at]
+
+    crane =
+      crane
+      |> Enum.with_index(1)
+      |> Enum.map(fn
+        {_, ^from} -> new_from
+        {_, ^to} -> new_to
+        {x, _} -> x
+      end)
+
+    perform_move({crane, [move - 1, from, to]})
+  end
+
+  def perform_moves({crane, []}), do: crane
+
+  def perform_moves({crane, [move | moves]}) do
+    crane = perform_move({crane, move})
+    perform_moves({crane, moves})
   end
 end
--- elixir/test/day5_test.exs
+++ elixir/test/day5_test.exs
@@ -4,15 +4,113 @@ defmodule AOC.Day5Test do
   alias AOC.Day5
   doctest Day5
 
-  @simple "day4/simple_input.txt"
-  @complex "day4/complex_input.txt"
+  @simple "day5/simple_input.txt"
+  @complex "day5/complex_input.txt"
 
-  test "Sums the correct amount per elf in simple input" do
+  test "Splits up input to two parts" do
+    parts =
+      @simple
+      |> Day5.get_input_parts()
+
+    assert length(parts) == 2
   end
 
-  test "finds largest in big text" do
+  test "parses crane section correctly" do
+    [crane, _] =
+      @simple
+      |> Day5.get_input_parts()
+
+    crane =
+      crane
+      |> Day5.parse_crane()
+
+    assert 3 == length(crane)
+    [one, two, three] = crane
+    assert 2 == length(one)
+    assert 3 == length(two)
+    assert 1 == length(three)
   end
 
-  test "find sum of largest three in big text" do
+  test "parses movement section correctly" do
+    [_, moves] =
+      @simple
+      |> Day5.get_input_parts()
+
+    moves
+    |> Day5.parse_movements()
+    |> assert_lists_equal([[1, 2, 1], [3, 1, 3], [2, 2, 1], [1, 1, 2]])
+  end
+
+  test "parses everything correctly" do
+    [crane, moves] =
+      @simple
+      |> Day5.get_input_parts()
+
+    crane = Day5.parse_crane(crane)
+    moves = Day5.parse_movements(moves)
+    assert length(crane) == 3
+    assert length(moves) == 4
+  end
+
+  test "performs movements correctly" do
+    {crane, moves} =
+      @simple
+      |> Day5.get_input_parts()
+      |> Day5.parse_input_parts()
+
+    [move1, move2, move3, move4] = moves
+
+    crane_1 = {crane, move1} |> Day5.perform_move()
+
+    [crane_1_1, crane_1_2, crane_1_3] = crane_1
+    # , "1"])
+    assert_lists_equal(crane_1_1, ["D", "N", "Z"])
+    # , "2"])
+    assert_lists_equal(crane_1_2, ["C", "M"])
+    # , "3"])
+    assert_lists_equal(crane_1_3, ["P"])
+
+    crane_2 = {crane_1, move2} |> Day5.perform_move()
+    [crane_2_1, crane_2_2, crane_2_3] = crane_2
+    # "1"])
+    assert_lists_equal(crane_2_1, [])
+    # , "2"])
+    assert_lists_equal(crane_2_2, ["C", "M"])
+    # , "3"])
+    assert_lists_equal(crane_2_3, ["Z", "N", "D", "P"])
+
+    crane_3 = {crane_2, move3} |> Day5.perform_move()
+    [crane_3_1, crane_3_2, crane_3_3] = crane_3
+    # , "1"])
+    assert_lists_equal(crane_3_1, ["M", "C"])
+    # "2"])
+    assert_lists_equal(crane_3_2, [])
+    # , "3"])
+    assert_lists_equal(crane_3_3, ["Z", "N", "D", "P"])
+
+    crane_4 = {crane_3, move4} |> Day5.perform_move()
+    [crane_4_1, crane_4_2, crane_4_3] = crane_4
+    # , "1"])
+    assert_lists_equal(crane_4_1, ["C"])
+    # , "2"])
+    assert_lists_equal(crane_4_2, ["M"])
+    # , "3"])
+    assert_lists_equal(crane_4_3, ["Z", "N", "D", "P"])
+
+    [one | _] = crane_4_1
+    [two | _] = crane_4_2
+    [three | _] = crane_4_3
+
+    assert one <> two <> three == "CMZ"
+  end
+
+  test "finds complex value" do
+    assert "BSDMQFLSP" ==
+             @complex
+             |> Day5.get_input_parts()
+             |> Day5.parse_input_parts()
+             |> Day5.perform_moves()
+             |> Enum.map(&Enum.at(&1, 0))
+             |> Enum.join()
   end
 end
