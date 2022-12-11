567ba5ddc01ff537ced9d8a801f5f19f53164c63
brehen <mkluften93@gmail.com>
2022-12-11 14:51:42 +0100

    ✨ D11P2 now supports 10000 rounds with new ways to manage worry

--- elixir/lib/day11.ex
+++ elixir/lib/day11.ex
@@ -1,11 +1,62 @@
 defmodule AOC.Day11 do
   import AOC.Utils, only: [read_input: 1]
 
+  @manage_stress 17 * 5 * 11 * 13 * 3 * 19 * 2 * 7
+  # @manage_stress 23 * 19 * 13 * 17
+
   @type monkey :: %{
           :items => [integer()],
           :operation => any(),
-          :test => {integer(), integer(), integer()}
+          :test => {integer(), integer(), integer()},
+          :position => integer(),
+          :inspected_items => integer()
         }
+
+  defp monkey_reducer("Monkey", acc), do: acc
+
+  defp monkey_reducer("Starting items: " <> items, acc) do
+    parsed_items =
+      items
+      |> String.split(", ", trim: true)
+      |> Enum.map(&String.to_integer/1)
+
+    Map.put(acc, :items, parsed_items)
+  end
+
+  defp monkey_reducer("Operation: new = old * old", acc),
+    do: Map.put(acc, :operation, &(&1 * &1))
+
+  defp monkey_reducer("Operation: new = " <> operation, acc) do
+    [_old, sign, add] = String.split(operation, " ")
+
+    add = String.to_integer(add)
+
+    operation_func =
+      case sign do
+        "*" -> &(&1 * add)
+        "+" -> &(&1 + add)
+        "-" -> &(&1 - add)
+      end
+
+    Map.put(acc, :operation, operation_func)
+  end
+
+  defp monkey_reducer("Test: divisible by " <> divisible_by, acc),
+    do: Map.put(acc, :test, {String.to_integer(divisible_by)})
+
+  defp monkey_reducer("If true: throw to monkey " <> target, acc) do
+    {div_by} = Map.get(acc, :test)
+    test = {div_by, String.to_integer(target)}
+    Map.put(acc, :test, test)
+  end
+
+  defp monkey_reducer("If false: throw to monkey " <> target, acc) do
+    {div_by, if_true} = Map.get(acc, :test)
+    test = {div_by, if_true, String.to_integer(target)}
+    Map.put(acc, :test, test)
+  end
+
+  defp monkey_reducer(_, acc), do: acc
 
   def get_monkeys(file_path) do
     read_input(file_path)
@@ -14,57 +65,75 @@ defmodule AOC.Day11 do
       monkey
       |> String.split("\n")
       |> Enum.map(&String.trim/1)
-      |> Enum.reduce(%{}, fn
-        "Monkey" <> _, acc ->
-          acc
-
-        "Starting items: " <> items, acc ->
-          parsed_items =
-            items
-            |> String.split(", ", trim: true)
-            |> Enum.map(&String.to_integer/1)
-            |> List.to_tuple()
-
-          Map.put(acc, :items, parsed_items)
-
-        "Operation: new = old * old", acc ->
-          Map.put(acc, :test, &(&1 * &1))
-
-        "Operation: new = " <> operation, acc ->
-          [_old, sign, add] = String.split(operation, " ")
-
-          add = String.to_integer(add)
-
-          operation_func =
-            case sign do
-              "*" -> &(&1 * add)
-              "+" -> &(&1 + add)
-              "-" -> &(&1 - add)
-            end
-
-          Map.put(acc, :operation, operation_func)
-
-        "Test: divisible by " <> divisible_by, acc ->
-          Map.put(acc, :test, {String.to_integer(divisible_by)})
-
-        "If true: throw to monkey " <> target, acc ->
-          {div_by} = Map.get(acc, :test)
-          test = {div_by, String.to_integer(target)}
-          Map.put(acc, :test, test)
-
-        "If false: throw to monkey " <> target, acc ->
-          {div_by, if_true} = Map.get(acc, :test)
-          test = {div_by, if_true, String.to_integer(target)}
-          Map.put(acc, :test, test)
-
-        _, acc ->
-          acc
-      end)
+      |> Enum.reduce(%{}, &monkey_reducer/2)
+    end)
+    |> Enum.with_index()
+    |> Enum.map(fn {monkey, index} ->
+      monkey = Map.put(monkey, :position, index)
+      Map.put(monkey, :inspected_items, 0)
     end)
   end
 
-  def get_highest(result) do
-    result |> Enum.max()
+  def perform_rounds(monkeys, manage_stress) do
+    monkeys
+    |> perform_round(manage_stress)
+  end
+
+  def perform_rounds(monkeys, 1, manage_stress) do
+    perform_rounds(monkeys, manage_stress)
+  end
+
+  def perform_rounds(monkeys, rounds, manage_stress) do
+    monkeys
+    |> perform_round(manage_stress)
+    |> perform_rounds(rounds - 1, manage_stress)
+  end
+
+  def perform_round(monkeys, manage_stress \\ 3) do
+    monkeys
+    |> Enum.reduce(monkeys, &perform_round(&1, &2, manage_stress))
+  end
+
+  def perform_round(monkey, monkeys, manage_stress) do
+    position = Map.get(monkey, :position)
+    items = Enum.at(monkeys, position) |> Map.get(:items)
+    inspected_items = Map.get(monkey, :inspected_items)
+    operation = Map.get(monkey, :operation)
+    {div_by, target_if_true, target_if_false} = Map.get(monkey, :test)
+
+    manage_stress = if manage_stress == 3, do: 3, else: @manage_stress
+
+    new_items =
+      items
+      |> Enum.map(fn item ->
+        # Monkey is crazy
+        new_item_worry = operation.(item)
+
+        new_item_worry =
+          if manage_stress == 3,
+            do: div(new_item_worry, 3),
+            else:
+              rem(new_item_worry, manage_stress)
+              |> floor()
+
+        new_item_passes_test = rem(new_item_worry, div_by) == 0
+
+        target = if new_item_passes_test, do: target_if_true, else: target_if_false
+        {new_item_worry, target}
+      end)
+
+    monkey = Map.put(monkey, :inspected_items, inspected_items + length(items))
+    current_monkey_after = Map.put(monkey, :items, [])
+
+    Enum.reduce(new_items, monkeys, fn {new_item, target}, acc ->
+      target_monkey = Enum.at(acc, target)
+      target_monkey_items = Map.get(target_monkey, :items)
+
+      new_monkey = Map.put(target_monkey, :items, [new_item | target_monkey_items])
+
+      List.replace_at(acc, target, new_monkey)
+    end)
+    |> List.replace_at(position, current_monkey_after)
   end
 
   def get_three_highest_sum(gnomes) do
--- elixir/test/day11_test.exs
+++ elixir/test/day11_test.exs
@@ -7,9 +7,6 @@ defmodule AOC.Day11Test do
   @simple "day11/simple_input.txt"
   @complex "day11/complex_input.txt"
 
-  test "Sums the correct amount per elf in simple input" do
-  end
-
   test "finds all monkeys from input" do
     assert 4 == Day11.get_monkeys(@simple) |> length
 
@@ -18,17 +15,60 @@ defmodule AOC.Day11Test do
 
   test "parses monkey parameters correctly" do
     [first_monkey, second_monkey, third_monkey, fourth_monkey] = Day11.get_monkeys(@simple)
-    assert Map.get(first_monkey, :items) == {79, 98}
+    assert Map.get(first_monkey, :items) == [79, 98]
     assert Map.get(first_monkey, :test) == {23, 2, 3}
 
     first_operation = Map.get(first_monkey, :operation)
     assert first_operation.(10) == 190
-    IO.inspect(first_operation.(10))
-    assert Map.get(second_monkey, :items) == {54, 65, 75, 74}
+    assert Map.get(second_monkey, :items) == [54, 65, 75, 74]
     assert Map.get(second_monkey, :test) == {19, 2, 0}
-    assert Map.get(third_monkey, :items) == {79, 60, 97}
+    second_operation = Map.get(second_monkey, :operation)
+    assert second_operation.(134) == 140
+    assert Map.get(third_monkey, :items) == [79, 60, 97]
     assert Map.get(third_monkey, :test) == {13, 1, 3}
-    assert Map.get(fourth_monkey, :items) == {74}
+    third_operation = Map.get(third_monkey, :operation)
+    assert third_operation.(30) == 900
+    assert Map.get(fourth_monkey, :items) == [74]
     assert Map.get(fourth_monkey, :test) == {17, 0, 1}
+    fourth_operation = Map.get(fourth_monkey, :operation)
+    assert fourth_operation.(10) == 13
+  end
+
+  test "monkey throws correctly" do
+    monkeys =
+      @simple
+      |> Day11.get_monkeys()
+
+    monkey_actions =
+      monkeys
+      |> Day11.perform_rounds(3)
+      |> Enum.map(&Map.get(&1, :items))
+      |> Enum.map(&Enum.sort/1)
+
+    assert_lists_equal(monkey_actions, [[20, 23, 26, 27], [25, 167, 207, 401, 1046, 2080], [], []])
+  end
+
+  test "monkey throws correctly after 20 rounds" do
+    [most_active, second_most_active | _who_cares] =
+      @simple
+      |> Day11.get_monkeys()
+      |> Day11.perform_rounds(20, 3)
+      |> Enum.map(fn %{:inspected_items => i} -> i end)
+      |> Enum.sort()
+      |> Enum.reverse()
+
+    assert most_active * second_most_active == 10_605
+  end
+
+  test "monkey throws correctly after 1000 rounds with no stress factor" do
+    [most_active, second_most_active | _who_cares] =
+      @complex
+      |> Day11.get_monkeys()
+      |> Day11.perform_rounds(10_000, 1)
+      |> Enum.map(fn %{:inspected_items => i} -> i end)
+      |> Enum.sort()
+      |> Enum.reverse()
+
+    assert most_active * second_most_active == 15_050_382_231
   end
 end
