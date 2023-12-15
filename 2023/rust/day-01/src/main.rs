f4f0f6691982efc3da45ba0392b2381be8d9a153
Marius Kluften <marius@kluften.dev>
2023-12-15 15:46:42 +0100

    solve day 1

--- 2023/rust/day-01/src/main.rs
+++ 2023/rust/day-01/src/main.rs
@@ -1,27 +1,71 @@
+use std::collections::HashMap;
+
 fn main() {
-    println!("Hello AOC!");
-}
+    let input = include_str!("../input2.txt");
+    let lines: Vec<&str> = input.lines().collect();
 
-fn part1(input: &str) -> &str {
-    input
-}
+    let number_map = create_number_map();
+    let mut sum = 0;
 
-fn part2(input: &str) -> &str {
-    input
-}
+    for line in lines {
+        let numbers = extract_numbers(line, &number_map);
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_part1() {
-        let input = include_str!("../input1.txt");
-        assert_eq!("123", part1(input));
+        if let (Some(&first), Some(&last)) = (numbers.first(), numbers.last()) {
+            sum += first * 10 + last;
+        }
     }
-    #[test]
-    fn test_part2() {
-        let input = include_str!("../input1.txt");
-        assert_eq!("", part2(input));
+
+    println!("Sum: {}", sum);
+}
+
+fn create_number_map() -> HashMap<&'static str, u32> {
+    [
+        ("zero", 0),
+        ("one", 1),
+        ("two", 2),
+        ("three", 3),
+        ("four", 4),
+        ("five", 5),
+        ("six", 6),
+        ("seven", 7),
+        ("eight", 8),
+        ("nine", 9),
+    ]
+    .iter()
+    .cloned()
+    .collect()
+}
+
+fn extract_numbers(line: &str, number_map: &HashMap<&str, u32>) -> Vec<u32> {
+    let mut numbers = Vec::new();
+    let mut current_word = String::new();
+
+    for ch in line.chars() {
+        if ch.is_ascii_digit() {
+            if !current_word.is_empty() {
+                if let Some(&number) = number_map.get(current_word.as_str()) {
+                    numbers.push(number);
+                }
+                current_word.clear();
+            }
+            numbers.push(ch.to_digit(10).unwrap());
+        } else if ch.is_alphabetic() {
+            current_word.push(ch);
+        } else {
+            if !current_word.is_empty() {
+                if let Some(&number) = number_map.get(current_word.as_str()) {
+                    numbers.push(number);
+                }
+                current_word.clear();
+            }
+        }
     }
+
+    if !current_word.is_empty() {
+        if let Some(&number) = number_map.get(current_word.as_str()) {
+            numbers.push(number);
+        }
+    }
+
+    numbers
 }
